e18c2e0d6f447cf4912a518b9f1152d6011c9245
lsfarm <58246081+lsfarm@users.noreply.github.com>
2021-01-25 12:01:18 -0600

    added signalStrength()

--- sprinklersystem-sim-i2crelay.ino
+++ sprinklersystem-sim-i2crelay.ino
@@ -15,6 +15,7 @@ V9   - (Table Widget) - used for showing value start and stop times
 V10  - (Terminal)
 V11  - (Time Input Widget) for advanced mode start time \
 V12  - Advanced mode zone run time (Numeric Input)      /
+V15  - Signal Strength
 V21  - Manual Valve switch input (Styled Button)
 V45  - 21-45 reserved for inputs (Styled Button)
 V101 - (Segment Switch) for advanced mode Case 0=Off - 1=EveryDay - 2=Everyother - 3=Every 3rd Day
@@ -44,20 +45,23 @@ enum  MODE { off = 1, manual = 2, automatic = 3, advanced = 4, unknown = 999 }; 
 MODE         mode        = unknown;
 void         setMode(MODE m);
 int timerNA = 99;
-int cycleAUTOtimer  = timerNA;
-int cycleADVANtimer = timerNA;
-/**** PowerMonitoring ***************************************************************************************************************/
+int cycleAUTOtimer      = timerNA;
+int AUTOtimerStartDelay = timerNA;
+int cycleADVANtimer     = timerNA;
+int setupdelay          = timerNA;  // delayed timer for some blynk stuff that won't load in setup
+/**** PowerMonitoring *************************************************************************************************************/
 bool hasVUSB(); 
 bool hadVUSB = false;
 WidgetLED pwLED(V8);
-
+/* I2C Relay 16 Channel  *********************************************************************************************************/
 #include <NCD16Relay.h>
 NCD16Relay R1;
-/* User Adjusted ***************************************************************************************************************/
+/* User Adjusted *****************************************************************************************************************/
 bool debugEnable = 1;
 bool sendTOblynk = 1;  //this sends v21-numZones button status to blynk in auto mode
 const int numZones = 12; // need const for advanSched[]
-/* Program Variables **********************************************************************************************************/
+/* Program Variables ************************************************************************************************************/
+int RSSI;
 long startTimeInSec;
 long zoneRunTime;
 long zoneRunTimeAsSec;
@@ -69,7 +73,6 @@ int previousDayADVAN = 0;
 int count = 1;
 int secoundcount;
 int counterADVAN;
-int setupdelay;  // delayed timer for some blynk stuff that won't load in setup
 int advanSched[numZones] [2]; //advanced schedule this holds values from V101 - (V101+numZones) in column [0] and keeps track of the scedule in column [1]  https://www.tutorialspoint.com/arduino/arduino_multi_dimensional_arrays.htm
 unsigned long runTimeADVAN[numZones]; //to do  plan to tie this to V150-175 for setting run times in advanced mode
 bool zoneStatus[numZones]; //this runs in updateBlynkTable() and is than use to correctly set zone turn off times when mode is changed in setMode() 
@@ -85,27 +88,25 @@ BLYNK_WRITE(V3) {  //Push Button that prints current time back to V2
     int button1 = param.asInt();
     if (button1) {
         Blynk.virtualWrite(V2, Time.format("%r %m/%d"));
+        signalStrength(); //get current stregth
+        Blynk.virtualWrite(V15, RSSI);
     }
 }
 BLYNK_WRITE(V5) { //Time Input Widget
-  startTimeInSec = param[0].asLong();
-  if (debugEnable) {
-      terminal.print("startTimeInSec: ");
-      terminal.println(startTimeInSec);
-      terminal.flush();
-   }
-  finishTimeCal();
+    startTimeInSec = param[0].asLong();
+    if (debugEnable) {
+        terminal.print("startTimeInSec: ");
+        terminal.println(startTimeInSec);
+        terminal.flush();
+    }
+    finishTimeCal();
 }
 BLYNK_WRITE(V6) {
     zoneRunTime = param[0].asLong();   //as minute
     zoneRunTime = 60 * zoneRunTime;    //convert minutes to seconds >>take this one out for debuging loops in secounds
     zoneRunTimeAsSec = zoneRunTime;    //used in finsihTimeCal()
     zoneRunTime = zoneRunTime * 1000L;  //convert secounds to  milli sec
-    if (debugEnable) {
-        terminal.print("zoneRunTime: ");
-        terminal.println(zoneRunTime);
-        terminal.flush();
-    }
+    if (debugEnable) { terminal.print("zoneRunTime: "); terminal.println(zoneRunTime); terminal.flush(); }
     finishTimeCal();
 }
 void finishTimeCal() {// V7  !!time.zone messes this up!!  terminal.print(Time.format("%D %r - "));
@@ -113,10 +114,7 @@ void finishTimeCal() {// V7  !!time.zone messes this up!!  terminal.print(Time.f
     long val = zoneRunTimeAsSec * numZones;
     long finTime = startTimeInSec + val; //this is sec
     Blynk.virtualWrite(V7, Time.format(finTime, "%r"));
-    //terminal.print("finTime: ");
-    //terminal.println(Time.format(finTime));
-    //terminal.println(Time.format("%D %r - "));
-    //terminal.flush();
+    //terminal.print("finTime: "); //terminal.println(Time.format(finTime)); //terminal.println(Time.format("%D %r - ")); //terminal.flush();
     Time.zone(timeOffset);
 }
 /////////************* **********/////////
@@ -126,23 +124,22 @@ BLYNK_WRITE(V0) {
   switch (mode = (MODE)param.asInt())
   {
     case off:
-      //Blynk.virtualWrite(V100, "Mode: OFF");
-      //terminal.println(mode);
-      //terminal.flush();
-      break;
+        //Blynk.virtualWrite(V100, "Mode: OFF");
+        //terminal.println(mode);//terminal.flush();
+        break;
     case manual: 
-      //Blynk.virtualWrite(V100, "Mode: Manual");
-      break;
+        //Blynk.virtualWrite(V100, "Mode: Manual");
+        break;
     case automatic: 
-      //Blynk.virtualWrite(V100, "Mode: Automatic");
-      break;
+        //Blynk.virtualWrite(V100, "Mode: Automatic");
+        break;
     case advanced:
-      break;
+        break;
     default:
-      mode = unknown;
-  }
+        mode = unknown;
+    }
   //sendInfoaftercommand();
-  setMode(mode);  
+    setMode(mode);  
   //if (mode != unknown) 
     //EEPROM.put(1, mode);                              //not used--may use if Wifi issues and blynk_connect fails
 }
@@ -179,7 +176,7 @@ void blynkWriteManual(int nr, int value) {
     case automatic: 
     case advanced:
       Blynk.virtualWrite(V21+nr, 0);
-      Blynk.notify("In Auto Mode - Selecting Manual Mode will dicountinue todays schedule");
+      Blynk.notify("In Auto Mode - Selecting Manual Mode will discountinue todays schedule");
       break;
       
     case unknown:
@@ -218,22 +215,14 @@ BLYNK_WRITE(V32) { blynkWriteManual(11, param.asInt()); }
 /////////************* **********/////////
 BLYNK_WRITE(V11) { //Time Input Widget  Zone Start Time
   startTimeInSecADVAN = param[0].asLong();
-  if(debugEnable){
-      terminal.print("startTimeInSecADVAN: ");
-      terminal.println(startTimeInSecADVAN);
-      terminal.flush();
-  }
+  if(debugEnable)  { terminal.print("startTimeInSecADVAN: "); terminal.println(startTimeInSecADVAN); terminal.flush(); }
   //finishTimeCal();
 }
 BLYNK_WRITE(V12) {
     zoneRunTimeADVAN = param[0].asLong();        //as minute
     zoneRunTimeADVAN = zoneRunTimeADVAN * 60;    //convert minutes to seconds
     zoneRunTimeADVAN = zoneRunTimeADVAN * 1000;  //converts seconds to millisec
-    if (debugEnable) {
-        terminal.print("zoneRunTimeADVAN: ");
-        terminal.println(zoneRunTimeADVAN);
-        terminal.flush();
-    }
+    if (debugEnable) { terminal.print("zoneRunTimeADVAN: "); terminal.println(zoneRunTimeADVAN); terminal.flush(); }
     //finishTimeCal();
 }
 BLYNK_WRITE(V101) {
@@ -261,12 +250,14 @@ void setup() { //wished could delay loop() if zone on time is in the past on res
     //Time.zone(-6);
     Blynk.begin(auth);
     Blynk.syncVirtual(V0, V1, V5, V6, V11, V12); // V101, V102, V103, V104, V105, V106, V107, V108, V109, V110, V111, V112);
+    pwLED.off(); //preset this to off in case power is off when it boots
     Blynk.virtualWrite(V9, "clr"); //clear the table
     setupdelay = timer.setTimeout(5000L, Blynk_init);
-    Blynk.notify("Power Outage Controller Has Restarted!");
-    R1.setAddress(1, 1, 1);
+    Blynk.notify("Battery Failure Controller Has Restarted!");
+    //moved to powerRegain()
+    /*R1.setAddress(1, 1, 1);
     if(R1.initialized){ terminal.println("Relay is ready"); terminal.flush(); }
-    else{ terminal.println("Relay not ready"); terminal.flush(); }
+    else{ terminal.println("Relay not ready"); terminal.flush(); }*/
 }
 
 void Blynk_init() { //running this in setup causes device to lockup
@@ -283,44 +274,80 @@ void Blynk_init() { //running this in setup causes device to lockup
         startTime[i] = Time.now(); //pass in current time to array for blynk table !!this actually happens in blynk.syncvirtual V0>>not sure how to do this
         stopTime[i] = Time.now();  //pass in current time to array for blynk table
     }
+    setupdelay = timerNA; 
 }
 
 void loop() {
     Blynk.run();
     timer.run();
     bool curVUSB = hasVUSB(); // for checking power supply status at USB
-    if (curVUSB != hadVUSB) { hadVUSB = curVUSB;    if(curVUSB) {pwLED.on();}   else{pwLED.off(); powerFail();}   }
+    if (curVUSB != hadVUSB) { hadVUSB = curVUSB;  if(curVUSB) {pwLED.on(); powerRegain();}   else{pwLED.off(); powerFail();}   }
     if (previousDay != Time.day() &&  Time.local() % 86400 >= startTimeInSec && mode == automatic) { //auto mode cycle
-        previousDay = Time.day();
-        count = 1; // in case mode gets changed
-        startcycleAUTO(); //timer already running check in this function
-   }
-       if (previousDayADVAN != Time.day() &&  Time.local() % 86400 >= startTimeInSecADVAN && mode == advanced && !timer.isEnabled(cycleADVANtimer)) { //advanced mode cycle
-        previousDayADVAN = Time.day();
-        terminal.println("inloop345678");
-        terminal.flush();
-        counterADVAN = 0; // in case mode gets changed
-        startcycleADVAN();
+        if (curVUSB) {
+            previousDay = Time.day();
+            count = 1; // in case mode gets changed
+            startcycleAUTO(); //timer already running check in this function
+            if(debugEnable) {terminal.println("startcycleAUTO() called from loop"); terminal.flush();}
+        }
+        else{
+            previousDay = Time.day();
+            Blynk.notify("Power Outage Auto Cycle Skipped Today");
+            if(debugEnable) { terminal.println("startcycleAUTO() skipped Power Outage"); terminal.flush();}
+        }
+    }
+    if (previousDayADVAN != Time.day() &&  Time.local() % 86400 >= startTimeInSecADVAN && mode == advanced && !timer.isEnabled(cycleADVANtimer)) { //advanced mode cycle
+        if(curVUSB) { //we have 120VAC power - run cycle
+            previousDayADVAN = Time.day();
+            counterADVAN = 0; // in case mode gets changed
+            startcycleADVAN();
+            if(debugEnable) { terminal.println("startcycleADVAN() called from loop"); terminal.flush();}
+        }
+        else{
+            previousDayADVAN = Time.day();
+            Blynk.notify("Power Outage Advanced Cycle Skipped Today");
+            if(debugEnable) { terminal.println("startcycleADVAN() skipped Power Outage"); terminal.flush();}
+        }
     }
 } //end loop
 
+void powerRegain() { //this also runs 1 time on reboot -- need some version of turnOff relays that where running when powerFail()
+    if(cycleAUTOtimer != 99) {Blynk.notify("Power Restored Auto Cycle canceled"); if(debugEnable) {terminal.println("power restored autocycle canceled"); terminal.flush();} }
+    else if (cycleADVANtimer != 99) {Blynk.notify("Power Restored Advanced Cycle canceled"); if(debugEnable) {terminal.println("power restored advancycle canceled"); terminal.flush();} }
+    else {Blynk.notify("Power Restored"); if(debugEnable) {terminal.println("power restored nothing canceled"); terminal.flush();}}
+    timer.deleteTimer(cycleAUTOtimer);
+    cycleAUTOtimer = timerNA;
+    timer.deleteTimer(cycleADVANtimer);
+    cycleADVANtimer = timerNA;
+    R1.setAddress(1, 1, 1); //restart comunication with relay board
+        for (int i = 0; i < numZones; i++) {
+            if(zoneStatus[i]){//if selected zone is on
+                R1.turnOffRelay(i+1);
+                Blynk.virtualWrite(V21+i, LOW);
+                updateBlynkTable(i, 0);
+            }
+        }
+    if (debugEnable) {
+        if(R1.initialized){ terminal.println(" Power Restored - Relay is ready"); terminal.flush(); }
+        else{ terminal.println("Relay not ready"); terminal.flush(); }
+    }
+}
 void powerFail() { //what should happen when VUSB goes dead
-    Blynk.notify("Power Outage Controller Has Restarted!");
+    if (debugEnable) {terminal.println("powerFail()"); terminal.flush();}
+    Blynk.notify("Power Outage!");
+    /// this doesn't work: R1.turnOffRelay(1);
 }
 
 void startcycleADVAN() {
-    terminal.print("co#: ");
-    terminal.println(counterADVAN);
+    if(debugEnable) { terminal.print(Time.format("%D %r - ")); terminal.print("co#: "); terminal.println(counterADVAN); }
     if (advanSched[counterADVAN][1] == 1) { //if selected zone needs to run today
         R1.turnOnRelay(counterADVAN + 1);     //turn  zone on
         updateBlynkTable(counterADVAN, 1);    //update Blynk table
         stopcycleADVAN();                     //wait zoneRunTime than shut it off
-        terminal.print("running advanced CYCLE Zone: ");
-        terminal.println(counterADVAN + 1);
-        terminal.flush();
+        if(debugEnable) { terminal.print("ADVANCYCLE Zone #"); terminal.print(counterADVAN + 1); terminal.println(" Running"); terminal.flush(); }
     }
-    else {
-        updateAdvanSchedArray();  //if selected zone doesn't need to run today, skip stop cycle and jump to array update.
+    else { //if selected zone doesn't need to run today, skip stop cycle and jump to array update.
+        if(debugEnable) {  terminal.print("ADVANCYCLE Zone #"); terminal.print(counterADVAN + 1); terminal.println(" Skipped"); terminal.flush(); }
+        updateAdvanSchedArray();  
     }
 }
   
@@ -358,7 +385,8 @@ void updateAdvanSchedArray() {
         }
         break;
       default:
-        terminal.println("Error in startcycle SwitchCase");
+        terminal.println("Error in updateAdvanSchedArray() SwitchCase"); terminal.flush();
+        Blynk.notify("Error 1 Contact Lance");
         break;
     }
     counterADVAN++;
@@ -370,15 +398,21 @@ void updateAdvanSchedArray() {
 
 void startcycleAUTO() {
     if (!timer.isEnabled(cycleAUTOtimer)) {// if the timers already running don't start another cycle (double up)
-        int delay1 = timer.setTimeout(1000L, [] () { R1.turnOnRelay(count); if(sendTOblynk) {Blynk.virtualWrite(V20+count, HIGH); updateBlynkTable(count - 1, 1); terminal.print("Zone ON Count: "); terminal.println(count); terminal.flush();}});
+        AUTOtimerStartDelay = timer.setTimeout(1000L, [] () { 
+            R1.turnOnRelay(count); 
+            if(sendTOblynk) Blynk.virtualWrite(V20+count, HIGH); 
+            updateBlynkTable(count - 1, 1); 
+            if (debugEnable) { terminal.print(Time.format("%D %r - ")); terminal.print("AUTOCYCLE Zone #"); terminal.print(count); terminal.println(" Running"); terminal.flush();}
+            AUTOtimerStartDelay = timerNA;
+        });
         cycleAUTOtimer = timer.setTimeout(zoneRunTime, [] () {
             cycleAUTOtimer = timerNA; //reset pointer for next round
             if (count != numZones) {//if we haven't reached the last zone
                 R1.turnOffRelay(count);
                 updateBlynkTable(count-1, 0);
                 if(sendTOblynk) {Blynk.virtualWrite(V20+count, LOW);}
-                terminal.print(Time.format("%D %r - "));
-                terminal.println(" - Manualtimerset");
+                if (debugEnable) {terminal.print(Time.format("%D %r - ")); terminal.print("AUTOCYCLE Zone #"); 
+                    terminal.print(count); terminal.println(" Stopped"); terminal.flush(); }
                 count++;
                 if (mode == automatic) {startcycleAUTO();}
             }
@@ -387,18 +421,14 @@ void startcycleAUTO() {
                 updateBlynkTable(numZones - 1, 0);
                 if(sendTOblynk) {Blynk.virtualWrite(V20+numZones, LOW);}
                 count = 1;
-                terminal.print(Time.format("%D %r - "));
-                terminal.println("last zone reached");
-                terminal.flush();
+                if (debugEnable) {terminal.print(Time.format("%D %r - ")); terminal.println("last AUTOCYCLE zone reached"); terminal.flush(); }
                 //previousDay = 1;  //used to debug
             }
         });
-        terminal.print(Time.format("%D %r - "));
-        terminal.println("bottom of startcycleAUTO()");
-        terminal.flush();
+        //if (debugEnable) { terminal.print(Time.format("%D %r - ")); terminal.println("bottom of startcycleAUTO()"); terminal.flush();}
     }
     else {
-        Blynk.notify("Error: Auto Timer Already Running");
+        Blynk.notify("Error 2: Auto Timer Already Running");
     }
 }
 
@@ -412,7 +442,7 @@ void setMode(MODE m) {
                 updateBlynkTable(i, 0);
             }
         }
-        terminal.println("inMODEoff");
+        terminal.println("setMode() = Off");
         terminal.flush();
         break;
       
@@ -427,7 +457,7 @@ void setMode(MODE m) {
                 updateBlynkTable(i, 0);
             }
        }
-        terminal.println("inMODEmanandauto");
+        terminal.println("setMode() = Man/Auto/Advan");
         terminal.flush();
     default: 
       break;
@@ -438,4 +468,18 @@ bool hasVUSB() { //checks if power supplied at USB this runs in loop() - bool cu
     uint32_t *pReg = (uint32_t *)0x40000438; // USBREGSTATUS
 
     return (*pReg & 1) != 0;
+}
+void signalStrength() {
+    terminal.print(Time.format("%r - "));
+    #if Wiring_Wifi 
+    int wifi = WiFi.RSSI().getStrength();
+    RSSI = wifi;
+    if(debugEnable) {terminal.print(wifi); terminal.println("% WiFi Strength"); terminal.flush(); }
+    #endif
+    #if Wiring_Cellular
+    CellularSignal sig = Cellular.RSSI();  //does this work??
+    RSSI = 999; //RSSI = sig; this isn't working
+    if(debugEnable) {terminal.print(sig); terminal.println("% Cell Strength"); terminal.flush(); }
+    //Log.info("Cellular signal strength: %.02f%%", sig.getStrength());
+    #endif
 }
